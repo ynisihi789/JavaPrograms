60f0a9c78e933da9e8af652996303945e04413fd
ynishi789 <ynishi789@gmail.com>
2026-01-06 14:33:42 +0545

    Addition of new class file

--- firstprogram.java
+++ firstprogram.java
@@ -1,7 +1,19 @@
-// A simple Java program
+// A simple Java program,variables in java
+import java.util.*;
 public class firstprogram{
     public static void main(String[] args) {
         // Print a message to the console
         System.out.println("Hello, kneesee! Welcome to Java programming.");
+        //input
+        Scanner sc = new Scanner(System.in);
+        String name =sc.nextLine();
+        System.out.println(name);
+        //nextInt ,nextFloat etc
+        int a =sc.nextInt();
+        int b = sc.nextInt();
+        int c=a+b;
+        System.out.println(c);
+
     }
 }
+
