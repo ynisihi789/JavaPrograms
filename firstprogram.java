3a9c8aa3ed608d7386041b4beb176eb07024d5d1
ynishi789 <ynishi789@gmail.com>
2026-01-06 13:36:27 +0545

    This is my first program

---
+++ firstprogram.java
@@ -0,0 +1,7 @@
+// A simple Java program
+public class firstprogram{
+    public static void main(String[] args) {
+        // Print a message to the console
+        System.out.println("Hello, kneesee! Welcome to Java programming.");
+    }
+}
